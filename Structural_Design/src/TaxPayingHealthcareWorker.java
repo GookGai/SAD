4bedbf05977702b88191ca4d9d84a4572c85d970
Warit Powtongsook <60293343+GookGai@users.noreply.github.com>
2022-04-01 10:21:01 +0700

    Add files via upload

---
+++ Structural_Design/src/TaxPayingHealthcareWorker.java
@@ -0,0 +1,12 @@
+public class TaxPayingHealthcareWorker extends HealthcareWorkerDecorator {
+
+    public TaxPayingHealthcareWorker(HealthcareWorker worker) {
+        super(worker);
+        System.out.println("Decorate " + worker.getName() + " with TaxPaying.");
+    }
+
+    @Override
+    public double getPrice() {
+        return super.getPrice() * 1.10;
+    }
+}
